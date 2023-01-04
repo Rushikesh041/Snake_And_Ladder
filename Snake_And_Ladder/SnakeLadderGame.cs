1cb87e6315367f7451299207965e3d6538a7734d
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-04 22:41:54 +0530

    [Rushi]UC7_TwoPlayers

--- Snake_And_Ladder/SnakeLadderGame.cs
+++ Snake_And_Ladder/SnakeLadderGame.cs
@@ -8,63 +8,119 @@ namespace Snake_And_Ladder
 {
     public class SnakeLadderGame
     {
+        int Position1 = 0;
+        int Position2 = 0;
+        int NoOfDiceRolled = 0;
+        int[] Players = { 1, 2 };
+        bool IsPlayer1 = true;
+        Random random = new Random();
         public void SnakeLadder()
         {
-            int Position1 = 0;
-            int count1 = 0;
-            int Num_Of_Players = 1;
-
-            Console.Write("Enter s to Start to play : ");
+            Console.Write("Press Enter to Start to play : ");
             string start = Console.ReadLine();
 
-            Console.WriteLine("Number of Players : " + Num_Of_Players);
+            Console.WriteLine("Number of Players : " + Players.Length);
             Console.WriteLine("Player 1 at position : " + Position1);
+            Console.WriteLine("Player 2 at position : " + Position2);
 
-            Random random = new Random();
-            while (Position1 != 100)
+
+
+            while (Position1 != 100 || Position2 != 100)
             {
-                int DiceNumber1 = random.Next(1, 7);
-                Console.WriteLine("Player rolled the Die ....");
-                Console.WriteLine("Dies turns to : " + DiceNumber1);
-
-                int Option1 = random.Next(1, 4);
-
-                switch (Option1)
+                int i = 0;
+                while (i < Players.Length)
                 {
-                    case 1:
-                        Console.WriteLine("you are at No Play option");
-                        Console.WriteLine("Current Position: " + Position1);
-                        break;
+                    Play(ref i);
 
-                    case 2:
-                        Console.WriteLine("you Get Move Ahead option");
-                        Console.WriteLine("Great! You get to Move Ahead, By " + DiceNumber1);
-                        Position1 = Position1 + DiceNumber1;
-                        Console.WriteLine("Current Position: " + Position1);
-                        break;
-
-                    case 3:
-                        Console.WriteLine("But you get Move back option");
-                        Console.WriteLine("Oops! you Need to Move Back, By" + DiceNumber1);
-                        Position1 = Position1 - DiceNumber1;
-                        Console.WriteLine("Current Position: " + Position1);
-                        break;
                 }
-                if (Position1 < 0)
+                if (Position1 == 100 || Position2 == 100)
                 {
-                    Console.WriteLine("Please Restart");
-                    Position1 = 0;
-                    continue;
-                }
-                if (Position1 == 100)
-                {
-                    Console.WriteLine("Player One wins");
+                    Console.WriteLine("\n**************** Playar : " + (Position1 == 100 ? " 1" : " 2") + " Wins *********************\n");
                     break;
                 }
-                count1++;
+
             }
-            Console.WriteLine("Total Number of Times Dies has turn to win is " + count1);
+            Console.WriteLine("Total Number of Times Dies Role is : " + NoOfDiceRolled);
         }
+        public void Play(ref int i)
+        {
+            Console.WriteLine("Player " + Players[i] + " turn");
+            int DiceNumber1 = random.Next(1, 7);
+            Console.WriteLine("Player rolled the Die ....");
+            Console.WriteLine("Dies turns to : " + DiceNumber1);
+
+            int Option = random.Next(1, 4);
+
+            switch (Option)
+            {
+                case 1:
+                    Console.WriteLine("you are at No Play option");
+
+                    break;
+
+                case 2:
+                    Console.WriteLine("you Get Move Ahead option");
+                    Console.WriteLine("Great! You get to Move Ahead, By : " + DiceNumber1);
+                    if (Players[i] == 1)
+                    {
+                        Position1 += DiceNumber1;
+                        if (Position1 > 100)
+                        {
+                            Position1 -= DiceNumber1;
+                            Console.WriteLine("Cannot Move Forward Remains in Same Position : " + Position1 + "\n");
+                        }
+                    }
+                    else if (Players[i] == 2)
+                    {
+                        Position2 += DiceNumber1;
+                        if (Position2 > 100)
+                        {
+                            Position2 -= DiceNumber1;
+                            Console.WriteLine("Cannot Move Forward Remains in Same Position : " + Position2 + "\n");
+                        }
+                    }
+                    break;
+
+                case 3:
+                    Console.WriteLine("But you get Move back option");
+                    Console.WriteLine("Oops! you Need to Move Back, By : " + DiceNumber1);
+                    if (Players[i] == 1)
+                    {
+                        Position1 -= DiceNumber1;
+                        if (Position1 < 0)
+                        {
+                            Console.WriteLine("Please Restart");
+                            Position1 = 0;
+                        }
+                    }
+                    else if (Players[i] == 2)
+                    {
+                        Position2 -= DiceNumber1;
+                        if (Position2 < 0)
+                        {
+                            Console.WriteLine("Please Restart");
+                            Position2 = 0;
+                        }
+                    }
+                    break;
+            }
+            Console.WriteLine("Player 1 Position : " + Position1);
+            Console.WriteLine("Player 2 Position : " + Position2);
+            Console.WriteLine("-----------------------------------------\n");
+            i++;
+            if (Option == 2 && i == 1 && Position1 != 100 && Position2 != 100)
+            {
+                i = 0;
+                Console.WriteLine("Player " + Players[i] + " Plays again");
+            }
+            if (Option == 2 && i == 2 && Position2 != 100 && Position1 != 100)
+            {
+                i = 1;
+                Console.WriteLine("Player " + Players[i] + " Plays again");
+            }
+            NoOfDiceRolled++;
+        }
+        
     }
 
 }
