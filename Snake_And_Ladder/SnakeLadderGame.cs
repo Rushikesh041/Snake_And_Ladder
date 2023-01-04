6d4b9b226ae5ebca0df9bce00e340be190fb5c7b
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-04 21:34:55 +0530

    UC2_RollDie

--- Snake_And_Ladder/SnakeLadderGame.cs
+++ Snake_And_Ladder/SnakeLadderGame.cs
@@ -13,11 +13,16 @@ namespace Snake_And_Ladder
             int position1 = 0;
             int Num_Of_Players = 1;
 
-            Console.WriteLine("Enter s to Start to play ");
+            Console.Write("Enter s to Start to play : ");
             string start = Console.ReadLine();
 
             Console.WriteLine("Number of Players : " + Num_Of_Players);
             Console.WriteLine("Player 1 at position : " + position1);
+
+            Random random = new Random();
+            int DieNumber1 = random.Next(1, 7);
+            Console.WriteLine("Player rolled the Die ....");
+            Console.WriteLine("Dies turns to : " + DieNumber1);
 
         }
     }
