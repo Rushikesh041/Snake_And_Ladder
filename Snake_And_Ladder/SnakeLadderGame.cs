66cd986a01e28fb718243191dc8947a9e216a9aa
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-04 21:52:03 +0530

    [Rushi]UC4_WinningPosition

--- Snake_And_Ladder/SnakeLadderGame.cs
+++ Snake_And_Ladder/SnakeLadderGame.cs
@@ -11,6 +11,7 @@ namespace Snake_And_Ladder
         public void SnakeLadder()
         {
             int Position1 = 0;
+            int Position2 = 0;
             int Num_Of_Players = 1;
 
             Console.Write("Enter s to Start to play : ");
@@ -20,29 +21,33 @@ namespace Snake_And_Ladder
             Console.WriteLine("Player 1 at position : " + Position1);
 
             Random random = new Random();
-            int DiceNumber1 = random.Next(1, 7);
-            Console.WriteLine("Player rolled the Die ....");
-            Console.WriteLine("Dies turns to : " + DiceNumber1);
-
-            int Option1 = random.Next(1, 4);
-
-            switch (Option1)
+            while (Position1 != 100)
             {
-                case 1:
-                    Console.WriteLine("you are at No Play option");
-                    break;
+                int DiceNumber1 = random.Next(1, 7);
+                Console.WriteLine("Player rolled the Die ....");
+                Console.WriteLine("Dies turns to : " + DiceNumber1);
 
-                case 2:
-                    Console.WriteLine("you Get Move Ahead option");
-                    Console.WriteLine("Great! You get to Move Ahead, By " + DiceNumber1);
-                    Position1 = Position1 + DiceNumber1;
-                    break;
+                int Option1 = random.Next(1, 4);
 
-                case 3:
-                    Console.WriteLine("But you get Move back option");
-                    Console.WriteLine("Oops! you Need to Move Back, By" + DiceNumber1);
-                    Position1 = Position1 - DiceNumber1;
-                    break;
+                switch (Option1)
+                {
+                    case 1:
+                        Console.WriteLine("you are at No Play option");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("you Get Move Ahead option");
+                        Console.WriteLine("Great! You get to Move Ahead, By " + DiceNumber1);
+                        Position1 = Position1 + DiceNumber1;
+                        break;
+
+                    case 3:
+                        Console.WriteLine("But you get Move back option");
+                        Console.WriteLine("Oops! you Need to Move Back, By" + DiceNumber1);
+                        Position1 = Position1 - DiceNumber1;
+                        break;
+                }
+
             }
         }
     }
